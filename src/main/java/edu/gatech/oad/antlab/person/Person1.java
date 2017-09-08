b225448a4cf371a0f2dc5bda771408bc3e8fc484
lastgituser <akikkeri3@gatech.edu>
2017-09-08 19:44:25 -0400

    I wrote Person1.java

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,7 +31,11 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-            return null;
+            String ans = "";
+            for (int i = 0; i < input.length(); i++) {
+                ans += input.charAt((i + 2) % input.length());
+            }
+            return ans;
 	}
 	
 	/**
